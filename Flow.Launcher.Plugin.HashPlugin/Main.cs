b6375a1e9b608b0f01cbd536b2a8cad83d5b70f6
Athied <cjsmith2000@hotmail.com>
2025-05-02 04:54:08 +1000

    code cleanup, added more options

--- Flow.Launcher.Plugin.HashPlugin/Main.cs
+++ Flow.Launcher.Plugin.HashPlugin/Main.cs
@@ -21,77 +21,47 @@ namespace Flow.Launcher.Plugin.HashPlugin
         public List<Result> Query(Query query)
         {
             string input = query.Search;
-            string md5Output = string.Empty;
-            string sha1Output = string.Empty;
-            string sha256Output = string.Empty;
 
             string iconPath = "Images/logo.png";
 
             byte[] inputBytes = Encoding.ASCII.GetBytes(input);
-
-            using (MD5 md5 = MD5.Create())
-            {
-                byte[] hashBytes = md5.ComputeHash(inputBytes);
-
-                md5Output = Convert.ToHexString(hashBytes).ToLower();
-            }
-
-            using (SHA1 sha1 = SHA1.Create())
-            {
-                byte[] hashBytes = sha1.ComputeHash(inputBytes);
-
-                sha1Output = Convert.ToHexString(hashBytes).ToLower();
-            }
-
-            using (SHA256 sha256 = SHA256.Create())
-            {
-                byte[] hashBytes = sha256.ComputeHash(inputBytes);
-
-                sha256Output = Convert.ToHexString(hashBytes).ToLower();
-            }
-
-            Result md5Result = new Result
-            {
-                Title = "md5",
-                SubTitle = md5Output,
-                IcoPath = iconPath,
-                Action = (ActionContext context) =>
-                {
-                    Clipboard.SetText(md5Output);
-                    return Clipboard.GetText() == md5Output;
-                }
-            };
-
-            Result sha1Result = new Result
-            {
-                Title = "sha1",
-                SubTitle = sha1Output,
-                IcoPath = iconPath,
-                Action = (ActionContext context) =>
-                {
-                    Clipboard.SetText(sha1Output);
-                    return Clipboard.GetText() == sha1Output;
-                }
-            };
-
-            Result sha256Result = new Result
-            {
-                Title = "sha256",
-                SubTitle = sha256Output,
-                IcoPath = iconPath,
-                Action = (ActionContext context) =>
-                {
-                    Clipboard.SetText(sha256Output);
-                    return Clipboard.GetText() == sha256Output;
-                }
-            };
+            byte[] inputBytesUTF8 = Encoding.UTF8.GetBytes(input);
 
             return new List<Result>()
             {
-                md5Result,
-                sha1Result,
-                sha256Result
+                MakeResult("md5", HashString(inputBytes, MD5.Create()), iconPath),
+                MakeResult("sha1", HashString(inputBytes, SHA1.Create()), iconPath),
+                MakeResult("sha256", HashString(inputBytes, SHA256.Create()), iconPath),
+                MakeResult("sha384", HashString(inputBytes, SHA384.Create()), iconPath),
+                MakeResult("sha512", HashString(inputBytes, SHA512.Create()), iconPath),
+                MakeResult("base64", Convert.ToBase64String(inputBytesUTF8), iconPath),
+                MakeResult("uppercase", input.ToUpper(), iconPath),
+                MakeResult("lowercase", input.ToLower(), iconPath),
             };
+        }
+
+        static Result MakeResult(string title, string output, string iconPath)
+        {
+            return new Result
+            {
+                Title = title,
+                SubTitle = output,
+                IcoPath = iconPath,
+                Action = (ActionContext context) =>
+                {
+                    Clipboard.SetText(output);
+                    return Clipboard.GetText() == output;
+                }
+            };
+        }
+
+        static string HashString(byte[] buffer, HashAlgorithm hashAlgorithm)
+        {
+            byte[] hashBytes = hashAlgorithm.ComputeHash(buffer);
+
+            hashAlgorithm.Dispose();
+
+            return Convert.ToHexString(hashBytes).ToLower();
         }
     }
 }
